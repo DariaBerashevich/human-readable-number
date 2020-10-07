07de652175454b92e2d055232c49549d142e16ed
DariaBerashevich <71172779+DariaBerashevich@users.noreply.github.com>
2020-10-07 13:51:31 +0300

    feat: repair solution

--- src/index.js
+++ src/index.js
@@ -1,47 +1,58 @@
-module.exports = function toReadable(number) {
-const wordsFromNumbers = {
-0: "zero",
-1: "one",
-2: "two",
-3: "three",
-4: "four",
-5: "five",
-6: "six",
-7: "seven",
-8: "eight",
-9: "nine",
-10: "ten",
-11: "eleven",
-12: "twelve",
-13: "thirteen",
-14: "fourteen",
-15: "fifteen",
-16: "sixteen",
-17: "seventeen",
-18: "eighteen",
-19: "nineteen",
-20: "twenty",
-30: "thirty",
-40: "forty",
-50: "fifty",
-60: "sixty",
-70: "seventy",
-80: "eighty",
-90: "ninety",
-100: "one hundred",
+module.exports =
+function toReadable(number) {
+let map = new Map([
+[0, "zero"],
+[1, "one"],
+[2, "two"],
+[3, "three"],
+[4, "four"],
+[5, "five"],
+[6, "six"],
+[7, "seven"],
+[8, "eight"],
+[9, "nine"],
+[10, "ten"],
+[11, "eleven"],
+[12, "twelve"],
+[13, "thirteen"],
+[14, "fourteen"],
+[15, "fifteen"],
+[16, "sixteen"],
+[17, "seventeen"],
+[18, "eighteen"],
+[19, "nineteen"],
+[20, "twenty"],
+[30, "thirty"],
+[40, "forty"],
+[50, "fifty"],
+[60, "sixty"],
+[70, "seventy"],
+[80, "eighty"],
+[90, "ninety"],
+[100, "one hundred"],
+[200, "two hundred"],
+[300, "three hundred"],
+[400, "four hundred"],
+[500, "five hundred"],
+[600, "six hundred"],
+[700, "seven hundred"],
+[800, "eight hundred"],
+[900, "nine hundred"],
+]);
+
+function recursion(number) {
+if(map.has(number)) return map.get(number);
+
+if (number<100) {
+let prompt = Math.floor(number/10)*10;
+number = number - prompt;
+return map.get(prompt) + " " + recursion(number);
 }
 
-if (number in wordsFromNumbers) return wordsFromNumbers[number];
+let prompthundreds = Math.floor(number/100)*100;
+number = number - prompthundreds;
+return map.get(prompthundreds) + " " + recursion(number);
+}
 
-const hundreds = Math.floor(number / 100);
-const dozens = Math.floor(number % 100 / 10);
-const digits = number % 100 % 10;
-
-const hundredsInText = hundreds ? `${wordsFromNumbers[hundreds]} hundred ` : '';
-if (!dozens && !digits) return hundredsInText.trim();
-
-const dozensInText = dozens && dozens >= 2 ? `${wordsFromNumbers[dozens * 10]} ` : wordsFromNumbers[dozens * 10 + digits]
-
-const digitsInText = wordsFromNumbers[digits]
-
-return (hundredsInText + dozensInText + (dozens >= 2 && digits ? digitsInText : '')).trim(); 
+return recursion(number);
+} 
